6d1010a8f76de2b139a740c7e7485e176072f527
Mark Holton <holtonma@Macintosh-69.local>
2009-10-30 19:16:03 -0700

    basic tags

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ActiveRecord::Base
   validates_presence_of :name, :title
   validates_length_of :title, :minimum => 5
   has_many :comments
+  has_many :tags
+
+  accepts_nested_attributes_for :tags, :allow_destroy => :true  ,
+    :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091031014231) do
+ActiveRecord::Schema.define(:version => 20091031015034) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(:version => 20091031014231) do
     t.string   "name"
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", :force => true do |t|
+    t.string   "name"
+    t.integer  "post_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
