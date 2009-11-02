20f56ebef0a38c83552f1fce2065068c41374a91
Mark Holton <holtonma@Macintosh-69.local>
2009-11-01 23:36:56 -0800

    formatting of links in li to include date printed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,6 +21,7 @@ class ArticlesController < ApplicationController
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @article }
+      format.iphone # show.iphone.erb
     end
   end
 
