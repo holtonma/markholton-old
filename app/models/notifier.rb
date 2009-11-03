effae695e5dbd4a93f8c154c6e2f07d7c5155475
Mark Holton <holtonma@Macintosh-69.local>
2009-11-03 13:18:39 -0800

    updated schedule, in the middle of adding some mailer items

---
+++ app/models/notifier.rb
@@ -0,0 +1,9 @@
+class Notifier < ActionMailer::Base
+  def signup_notification(user, message)
+    recipients "#{user.name} <#{user.email}>"
+    from       "IrishOptimist.com"
+    subject    "this is a test email"
+    sent_on    Time.now
+    body       { :user => user, :url => activate_url(user.activation_code, :host => user.site.host }
+  end
+end
