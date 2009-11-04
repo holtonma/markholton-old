6d8e534da3dc6d9557b6f57db852b60941655887
Mark Holton <holtonma@Macintosh-69.local>
2009-11-04 10:50:30 -0800

    rollback limit 5 (goal was on association, not newspapers)

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.find(:all, :include => [:comments])
-    @news = Newspaper.find(:all, :include => [:articles], :order => "articles.printed_date DESC", :limit => 5)
+    @news = Newspaper.find(:all, :include => [:articles], :order => "articles.printed_date DESC")
     
     respond_to do |format|
       format.html # index.html.erb
