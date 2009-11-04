e61fd67079a3f4c83d37a8d4dbfe5b64f043d815
Mark Holton <holtonma@Macintosh-69.local>
2009-11-04 10:48:59 -0800

    limiting articles to last 5, ordering by printed_date

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @posts = Post.find(:all, :include => [:comments])
-    @news = Newspaper.find(:all, :include => [:articles])
+    @news = Newspaper.find(:all, :include => [:articles], :order => "articles.printed_date DESC", :limit => 5)
     
     respond_to do |format|
       format.html # index.html.erb
