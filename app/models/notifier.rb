e4cbb89b9cdafa643c8d0d6632bb5263dcc55824
Mark Holton <holtonma@Macintosh-69.local>
2010-04-07 10:42:13 -0700

    turning off caching

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,7 +1,7 @@
 class Notifier < ActionMailer::Base
   # def signup_notification(user, message)
   #   recipients "#{user.name} <#{user.email}>"
-  #   from       "IrishOptimist.com"
+  #   from       "MarkHolton.com"
   #   subject    "this is a test email"
   #   sent_on    Time.now
   #   body       { :user => user, :url => activate_url(user.activation_code, :host => user.site.host }
