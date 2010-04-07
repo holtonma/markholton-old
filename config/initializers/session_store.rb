327a694d2c515e30c9c057fef7c60de9d9916ef1
Mark Holton <holtonma@Macintosh-69.local>
2010-04-07 11:23:06 -0700

    latest

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_IrishOptimist_session',
+  :key         => '_MarkHolton_session',
   :secret      => 'f2c9fe444241fb163ff9ce5d9c424a8ef937ad67140810eb5b838545b3dc3f5c4a1f504dea239872d0f62b70bb920de91cdeac785c0b12dc5ab753c27c399819'
 }
 
